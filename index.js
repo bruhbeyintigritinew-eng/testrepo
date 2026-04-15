cf44e62806beba43e3cf4ee6cf8faded16ba0dff
bruhbeyintigritinew-eng <bruhbeyintigritinew@gmail.com>
2026-04-15 19:45:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const app = {}
+Hey mate
 
-app.get("/user", (req, res) => {
-    const connStr = "Server=tcp:myserver.database.windows.net,1433;Initial Catalog=mydb;Persist Security Info=False;User ID=myuser;Password=$uperSecret123!@#";
-    const username = req.query.username
-    const unsafeQuery = `SELECT * FROM users WHERE username = '${username}'`
-    sql.connect(connStr).query(unsafeQuery, (err, result) => {
-        res.status(200).send(result)
-    })
-})
+if you able to read this,don't respond me back,just say hello
